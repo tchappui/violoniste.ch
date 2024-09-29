a49286ba9ffcda8e1e6736c3d5ec61e99310f738
tchappui <tchappui@gmail.com>
2024-09-29 16:47:20 +0200

    Version finale

---
+++ src/data/comments.js
@@ -0,0 +1,95 @@
+// data/comments.js
+const comments = [
+    {
+      text: `Je n'avais plus touché mon violon depuis l'adolescence, soit presque 30 ans.
+      Je suis ravie d'avoir repris des cours grâce à l'enseignement de Madame Chappuis.
+      Les leçons sont structurées de manière à remettre en place les indispensables
+      bases techniques, mais surtout à (re)trouver rapidement le plaisir de jouer.
+      C'est mon cas. La disponibilité de Madame Chappuis est très appréciable pour
+      quelqu'un qui est actif professionnellement, notamment s'agissant de la durée
+      et de la fréquence des leçons. Sa méthode d'enseignement s'adapte à mes qualités
+      et mes défauts. J'apprécie particulièrement son état d'esprit positif, ses
+      encouragements, sa passion. Son enseignement me permet de faire rapidement des
+      progrès. En bref, que du plaisir.`,
+      author: "Jean-Baptiste",
+    },
+    {
+      text: `Ayant été élève d’Evelyne pendant environ 15 ans, elle m’a fait voyager à
+      travers les différentes époques et styles musicaux en me faisant jouer du violon
+      en solo, en duo ou en organisant de la musique de chambre avec les autres élèves
+      de sa classe. En plus de ses compétences musicales de violoniste, Evelyne m’a
+      également appris à être persévérante, organisée et régulière grâce au violon
+      dans mon apprentissage de manière générale. C’est une enseignante passionnée,
+      appliquée et très engagée dans son travail avec les jeunes. Grâce à toutes ses
+      techniques et ses conseils adaptés à chacun de ses élèves, elle les encourage et
+      motive toujours à atteindre des objectifs précis. De plus, Evelyne met en place
+      régulièrement des cours collectifs avec ses élèves afin qu’ils puissent apprendre
+      et profiter les uns des autres. Enfin, l’enseignement d’Evelyne est varié afin
+      que tous ses élèves puissent apprendre le violon au mieux et dans les meilleures
+      conditions.`,
+      author: "Eva Nowak",
+    },
+    {
+      text: `Evelyne Chappuis offre un enseignement du violon soigneux et motivant, en
+      mettant un point d’honneur sur la maîtrise de l’instrument et l’expressivité
+      musicale. Etant son élève depuis plus d’une dizaine d’années, j’apprécie
+      particulièrement son professionnalisme, sa pédagogie, et son engagement auprès
+      des élèves. Sa méthode d’enseignement, qui s’adapte à l’élève, permet de
+      progresser en y trouvant beaucoup de plaisir !`,
+      author: "Klara Bourban",
+    },
+    {
+      text: `Mme Evelyne est très sympa, très gentille, très patiente avec les enfants.
+      En cas de difficulté, elle s'investit beaucoup pour rendre les choses plus
+      faciles. Elle sait motiver et encourager ses élèves. Ma fille suit des leçons
+      de violon avec elle, elle en est très contente. Chaque jour elle répète les
+      leçons à la maison et elle se sent vraiment très motivée à continuer avec
+      Evelyne.`,
+      author: "Shada Barjo, maman de Marina",
+    },
+    {
+      text: `L’enthousiasme et l’investissement de Mme Chappuis dans les cours de violon
+      adressés à notre fille sont réels et portent leurs fruits. Les cours de musique
+      de chambre sont aussi un avantage que l’on apprécie bien, car la musique qui se
+      partage est plus enrichissante pour les élèves et les parents qui écoutent.
+      Notre fille a un plaisir palpable à jouer ses partitions. Bien sûr il faut
+      travailler, et le goût du travail à fond est aussi un atout dans l’enseignement
+      de Mme Chappuis. Nous recommandons cette professeure passionnée, qui sait
+      largement encourager et féliciter notre fille.`,
+      author: "Mme Fillatre, maman de Esther",
+    },
+    {
+      text: `Évelyne est très sympathique et compétente. Elle transmet habilement les
+      bases du violon. J'ai été étonné des progrès que j'ai fait en peu de temps.
+      Evelyne est très enthousiaste et encourageante. A chaque leçon, elle souligne
+      ce que je fais bien et ce qui fonctionne bien. Elle donne inlassablement des
+      feedback positifs et les notes pour que je puisse les revoir à la maison.
+      Dans son enseignement, Évelyne attache beaucoup d'importance à la création
+      de bases solides et à la transmission de son plaisir à jouer du violon.`,
+      author: "Patrick",
+    },
+    {
+      text: `Evelyne a deux casquettes. Celle d’artiste et celle de professeur, elles sont
+      de la même couleur, celle de la passion et du partage. Un partage de haute
+      qualité. Évelyne donne beaucoup dans ses cours. Elle nous fait avancer pas à
+      pas, consolide les acquis, nous encourage. Elle est aussi contente que nous,
+      quand elle sent que nous avons intégré ses apprentissages. C'est très
+      encourageant. Elle est exigeante afin de ne pas brûler les étapes. Elle a à
+      cœur de nous donner les bases essentielles pour progresser par la suite avec
+      plus de plaisir. J'ai beaucoup apprécié son accompagnement pour m'aider à
+      ajuster mes gestes et mon tonus, étapes essentielles pour essayer de trouver
+      un joli son qui nous motivera pour apprendre de nouveaux morceaux. Suivre
+      ses cours est une garantie de progression bien intégrée qui nous suivra
+      toujours.`,
+      author: "Jean-Marc",
+    },
+    {
+      text: `Evelyne a une dynamique d'enseignement très positive et très engagée envers
+      les élèves. Ce qui est très remarquable. J'ai appris énormément de choses en
+      très peu de temps, car elle est très efficace. Sa manière d'enseigner est
+      très simple à suivre. C'était une expérience très enrichissante.`,
+      author: "Iris",
+    },
+  ];
+  
+  export default comments;
